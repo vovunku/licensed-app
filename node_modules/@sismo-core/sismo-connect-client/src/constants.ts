f7adf5a4c308a31a6a06054c17bdb782967204bf
Vladimir Kukharenko <kukharenko.va@phystech.edu>
2023-05-13 21:44:47 +0200

    gitignore fix

--- node_modules/@sismo-core/sismo-connect-client/src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const PROD_VAULT_APP_BASE_URL = `https://vault-beta.sismo.io`;
-export const DEV_VAULT_APP_BASE_URL = `https://dev.vault-beta.sismo.io`;
--- node_modules/@sismo-core/sismo-connect-client/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./sismo-connect";
-export * from "./types";
-export * from "./constants";
-export * from "./common-types";
-export * from "./sdk";
-export * from "./api";
